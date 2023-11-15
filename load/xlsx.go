97bdb39120b016d783a78c9ddf80c79ac7d844a0
starme <zj_ng@126.com>
2023-11-15 16:51:40 +0800

    add read stream

--- load/xlsx.go
+++ load/xlsx.go
@@ -15,6 +15,39 @@ type excel struct {
 	colCount  int
 }
 
+func (e excel) ReadStream() (row [][]string, err error) {
+	var f *excelize.File
+	// 读取excel
+	f, err = excelize.OpenReader(e.file)
+	if err != nil {
+		err = errors.New("读取Excel文件失败：" + err.Error())
+		return
+	}
+	// 关闭文件流
+	defer f.Close()
+	// 读取Excel值
+	var rowsGet [][]string
+	rowsGet, err = f.GetRows(e.sheetName)
+	if err != nil {
+		err = exerrors.ErrSheetNotExist{SheetName: e.sheetName}
+		return
+	}
+	for _, row := range rowsGet {
+		if e.colCount == 0 {
+			e.colCount = len(row)
+		}
+		rowData := make([]string, e.colCount, e.colCount)
+		for i, cell := range row {
+			if i+1 > e.colCount {
+				continue
+			}
+			rowData[i] = formatCellValue(cell)
+		}
+		rows = append(rows, rowData)
+	}
+	return
+}
+
 func (e excel) Read() (rows [][]string, err error) {
 	var f *excelize.File
 	// 读取excel文件
