97dd233ad79e1fbf53fdd4b6f661bdd6bf78b5fd
starme <zj_ng@126.com>
2023-11-15 17:20:46 +0800

    fix steam

--- load/factory.go
+++ load/factory.go
@@ -9,6 +9,7 @@ import (
 
 type Reader interface {
 	Read() (rows [][]string, err error)
+	ReadStream() (rows [][]string, err error)
 }
 
 const (
--- template/reader.go
+++ template/reader.go
@@ -155,7 +155,7 @@ func LoadHttpExcel(fh *multipart.FileHeader, sheetName string, colCount int) (ro
 		return
 	}
 
-	rows, err = reader.Read()
+	rows, err = reader.ReadSteam()
 	return
 }
 
